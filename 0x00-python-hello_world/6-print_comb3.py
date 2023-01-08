8f21b6857d807519cd526ff4884687bed20bd1b5
spindouken <spindouken@gmail.com>
2023-01-07 23:28:26 -0600

    massive local push of working code

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for x in range(10):
+    for z in range(10):
+        if z <= x:
+            continue
+        elif x != 8 or x != 9:
+            print('{}{}'.format(x, z), end=", ")
+        else:
+            print('{}{}'.format(x, z))
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for x in str:
+        if ord(x) >= 97 and ord(x) < 123:
+            result = result + chr(ord(x) - 32)
+        else:
+            result = result + x
+    print('{}'.format(result))
+	
