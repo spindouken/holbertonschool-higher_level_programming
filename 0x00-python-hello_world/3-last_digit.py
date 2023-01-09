7206a65431431845de32ea5dc8b79a090bc7efdf
spindouken <spindouken@gmail.com>
2023-01-09 16:40:37 -0600

    update

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    lastDigit = number % 10
+    lastDigit = -number % 10
     lastDigit = -lastDigit
 else:
     lastDigit = number % 10
