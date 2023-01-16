ad57024f2fb7ebaf42b1d9451758df47db133387
spindouken <spindouken@gmail.com>
2023-01-16 13:59:17 -0600

    revert

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(' '.join([f"{item}" for item in row]))
+        for item in row:
+            print("{:d}".format(item), end=" ")
+        print()
