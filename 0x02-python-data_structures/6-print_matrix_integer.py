a68ba5916dbe9b388dc1b192b785d5507fd6c9a6
spindouken <spindouken@gmail.com>
2023-01-16 13:53:14 -0600

    different format attempt

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for z in x:
-            print("{} ".format(z), end="")
-        print()
+    for row in matrix:
+        print(' '.join([f"{item}" for item in row]))
