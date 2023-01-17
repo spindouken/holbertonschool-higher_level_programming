a4f70b1ecc20372edebc9bd83fdc2138aee8d01e
spindouken <spindouken@gmail.com>
2023-01-17 10:36:26 -0600

    no f-string.. str.format()

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(' '.join([f"{item}" for item in row]))
-    
+        print(' '.join(["{:d}".format(item) for item in row]))
