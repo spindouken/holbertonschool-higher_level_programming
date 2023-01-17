979ada960ecf6fddcb7e1324a419726c5469fa82
spindouken <spindouken@gmail.com>
2023-01-17 10:17:59 -0600

    all working code

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square class is square"""
+
+
+class Square:
+    """it's a square with a size"""
+    def __init__(self, size=0):
+        self.size = size
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)
