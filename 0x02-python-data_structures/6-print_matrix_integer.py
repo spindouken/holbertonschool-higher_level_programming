6498ef2c303cb26b67d71f61f9347a2447dd6802
spindouken <spindouken@gmail.com>
2023-01-17 10:34:07 -0600

    reformat using f-string

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for item in row:
-            print("{:d}".format(item), end=" ")
-        print()
+        print(' '.join([f"{item}" for item in row]))
+    
