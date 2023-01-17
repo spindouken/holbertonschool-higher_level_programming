ba16d5f423e201b84e2ac1209771029e1db031d8
spindouken <spindouken@gmail.com>
2023-01-17 10:23:39 -0600

    compliance complete

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -6,11 +6,11 @@ class Square:
     """it's a square with a size"""
     def __init__(self, size=0):
         self.size = size
-    
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if type(value) != int:
