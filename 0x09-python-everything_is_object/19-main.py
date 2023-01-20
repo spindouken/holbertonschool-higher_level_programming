2d1fa88039dae2ab5a2f6e4a2fadad24d275408f
spindouken <spindouken@gmail.com>
2023-01-19 19:48:53 -0600

    whoops mixed files

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def copy_list(l):
-    return l[:]
+copy_list = __import__('19-copy_list').copy_list
+
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
