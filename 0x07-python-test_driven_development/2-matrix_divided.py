13e1bca0176f51e21c27603c821f2d7dbf8a0ce1
spindouken <spindouken@gmail.com>
2023-01-24 12:19:41 -0600

    matrix omega test file update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,9 +23,9 @@ def matrix_divided(matrix, div):
     for row in range(len(matrix)):
         if len(matrix[0]) != len(matrix[row]):
             raise TypeError("Each row of the matrix must have the same size")
-        for value in range(len(matrix[row])):
-            if type(matrix[row][value]) != int and\
-                    type(matrix[row][value]) != float:
+        for x in range(len(matrix[row])):
+            if type(matrix[row][x]) != int and\
+                    type(matrix[row][x]) != float:
                 raise TypeError("matrix must be a matrix (list of lists) of "
                                 "integers/floats")
-    return [[round(value / div, 2) for value in row] for row in matrix]
+    return [[round(x / div, 2) for x in row] for row in matrix]
