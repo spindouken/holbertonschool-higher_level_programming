eff62b0b0167aaf9c0bf25c4e6b6b07e8daa071a
spindouken <spindouken@gmail.com>
2023-01-23 22:41:11 -0600

    reformat as type chech and string literal

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,13 +4,16 @@
 
 def say_my_name(first_name:str, last_name:str =""):
     """
-    This function takes in two string arguments: first_name and last_name, and prints out a string that says "My name is {first_name} {last_name}"
+    This function takes in two string arguments: first_name and last_name
+
     Args:
         first_name (str): The first name of the person
         last_name (str): The last name of the person
     Raises:
         TypeError: When the first_name or the last_name is not of type string
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
