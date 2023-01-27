9772704be08542fb905ece29da2200c17ea1abc1
spindouken <mlc9181209@gmail.com>
2023-01-26 20:30:57 -0600

    compliance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "check if object shares the same class"
 
+
 def is_same_class(obj, a_class):
     """returns true if the object shares the same instance of specified class
     Return false if different instance"""
