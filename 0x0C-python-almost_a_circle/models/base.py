f03780ceee571b2019e716fc135700ff0e187b21
spindouken <mlc9181209@gmail.com>
2023-01-28 12:48:46 -0600

    compliance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,11 +4,11 @@ Based module contains based class Base
 Based class Base contains private class '__nb_objects'
     that keeps track of the number of instances created
     and a public instance attribute 'id' that is either
-    assigned or auto-generated based 
+    assigned or auto-generated based
     on the '__nb_objects' attribute.
     Also, contains class constructor __init__.
-    
 """
+
 
 class Base:
     """
