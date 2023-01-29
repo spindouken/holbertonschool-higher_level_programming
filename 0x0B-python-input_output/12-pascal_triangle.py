a64b2efdf0f89b8f30f9f9e48c0245b637344a67
spindouken <mlc9181209@gmail.com>
2023-01-29 15:20:29 -0600

    pascal's triangle and pychache upload

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,17 +4,19 @@
 
 def pascal_triangle(n):
     """returns a list of lists of integers
-    representing the Pascal’s triangle of n"""
+    representing the Pascal’s triangle of n
+    n: number of rows
+    """
 
     if n <= 0:
         return ""
 
-    triangle = [[1]]
+    pascals_triangle = [[1]]
     for current_row in range(1, n):
         row = [1]
-        prev_row = triangle[current_row - 1]
-        for el in range(1, current_row):
-            row.append(prev_row[el] + prev_row[el - 1])
+        previous_row = pascals_triangle[current_row - 1]
+        for element in range(1, current_row):
+            row.append(previous_row[element] + previous_row[element - 1])
         row.append(1)
-        triangle.append(row)
-    return triangle
+        pascals_triangle.append(row)
+    return pascals_triangle
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,15 +4,12 @@
 
 class Student:
     """Student class"""
-
     def __init__(self, first_name, last_name, age):
         """Initialize student class"""
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """retrieves a dictionary representation of a Student instance"""
-
         return self.__dict__
