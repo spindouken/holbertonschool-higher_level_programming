377c5f627224ad1f6d9fb56a6294db916872da4d
spindouken <mlc9181209@gmail.com>
2023-01-29 15:40:52 -0600

    format change

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
-
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        for text in f:
+            print(text, end="")
