52813593cfc6cc363c4379277a045d1d0edc19a7
spindouken <mlc9181209@gmail.com>
2023-01-29 23:53:46 -0600

    task 18, pep8 cleared

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -54,3 +54,13 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        else:
+            dummy_instance = cls(1)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
