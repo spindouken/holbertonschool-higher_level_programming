b1b6f145644bbbf8574c826eead17b9763e57c9d
spindouken <mlc9181209@gmail.com>
2023-01-29 18:14:56 -0600

    trackback.. code not running

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,18 +97,6 @@ class Rectangle(Base):
                 print('#', end="")
             print()
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
-
     def __str__(self):
         """
         Overrides the built-in __str__ method to return a string in the format:
