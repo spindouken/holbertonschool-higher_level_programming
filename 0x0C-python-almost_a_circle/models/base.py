30c143a57526408086a13d7ee1012e07edf4a2c2
spindouken <mlc9181209@gmail.com>
2023-01-29 23:38:23 -0600

    task 15 attempt, code working

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries is []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
