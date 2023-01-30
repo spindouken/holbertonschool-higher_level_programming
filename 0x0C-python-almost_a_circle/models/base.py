75f5cccf7c0efdc8427431c461594317f3f2e954
spindouken <mlc9181209@gmail.com>
2023-01-29 23:40:42 -0600

    forgot static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,9 +26,22 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries is []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a filei"""
+
+        new_list = []
+        new_file = cls.__name__ + ".json"
+        if list_objs is not None:
+            for el in list_objs:
+                new_list.append(el.to_dictionary())
+        with open(new_file, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(new_list))
