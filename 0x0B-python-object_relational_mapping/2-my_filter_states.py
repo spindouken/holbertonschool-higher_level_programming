cdf6d0a7520f15220684d6a65057ef1d305feb4a
spindouken <mlc9181209@gmail.com>
2023-02-27 17:49:30 -0600

    attempt 2

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -7,18 +7,28 @@ import sys
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
-    cr = db.cursor()
-    query = " ".join([
-        "SELECT * FROM states",
-        "WHERE name LIKE BINARY '{}'",
-        "ORDER BY id ASC",
-        ]).format(sys.argv[4])
-    cr.execute(myQuery)
-    res = cr.fetchall()
-    for rows in res:
-        print(rows)
-    cr.close()
-    db.close()
+# Connect to MySQL database using the credentials provided as arguments
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=sys.argv[1], passwd=sys.argv[2],
+                     db=sys.argv[3], charset="utf8")
+
+# Create a cursor object to execute SQL queries
+cursor = db.cursor()
+
+# Use format to create the SQL query with the user input
+query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"\
+        .format(sys.argv[4])
+
+# Execute the SQL query
+cursor.execute(query)
+
+# Fetch all the rows of the query result
+rows = cursor.fetchall()
+
+# Display the query results
+for row in rows:
+    print(row)
+
+# Close the cursor and database connections
+cursor.close()
+db.close()
