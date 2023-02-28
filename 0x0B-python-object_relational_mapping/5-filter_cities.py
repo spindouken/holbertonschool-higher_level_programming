dc3e7369b58dc237a55d02e4269c6f8d3a45b7e0
spindouken <mlc9181209@gmail.com>
2023-02-27 19:05:58 -0600

    readability update

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument and
-lists all cities of that state, using hbtn_0e_4_usa"""
+
+"""
+This script takes in the name of a state as an argument and
+lists all cities of that state, using hbtn_0e_4_usa.
+"""
+
 import MySQLdb
 import sys
 
-
+# Check if this script is being run directly and not being imported
 if __name__ == '__main__':
+    
+    # Connect to the database using given credentials
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
-    cr = db.cursor()
+    # Build the SQL query
     query = " ".join([
         "SELECT cities.name FROM cities",
         "INNER JOIN states ON states.id = cities.state_id",
         "WHERE states.name LIKE BINARY '{}'",
         "ORDER BY cities.id",
-        ]).format(sys.argv[4])
-    cr.execute(query)
-    res = cr.fetchall()
-    strRes = ', '.join([i[0] for i in res])
-    print(strRes)
-    cr.close()
+    ]).format(sys.argv[4])
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Join the results into a comma-separated string
+    results_str = ', '.join([i[0] for i in results])
+
+    # Print the results string
+    print(results_str)
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
