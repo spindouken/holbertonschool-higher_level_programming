3b584e37e1e0af84ed5a335d9b7ddc5062b5046b
spindouken <mlc9181209@gmail.com>
2023-02-28 16:06:01 -0600

    pycodestyle compliance

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
 
     # Connect to MySQL database using the credentials provided as arguments
     db = MySQLdb.connect(host="localhost", port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2],
-                        db=sys.argv[3], charset="utf8")
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
 
     # Create a cursor object to execute SQL queries
     cursor = db.cursor()
