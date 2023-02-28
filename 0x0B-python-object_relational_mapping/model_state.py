5633c9bd915838e9e7303588585e588cf0020ead
spindouken <mlc9181209@gmail.com>
2023-02-27 18:28:33 -0600

    6 working code

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that containes the class definition of a State and instance Base"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that inherists from Base declarative"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
