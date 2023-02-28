77fe3fbf88e42d5a26318d2cf792e30946072a8d
spindouken <mlc9181209@gmail.com>
2023-02-28 16:04:46 -0600

    syntax fix attempt #3

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,10 +8,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Create an engine to connect to the database using the command line arguments
+    # Create an engine to connect to the database using the cli arguments
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     # Create the table if it doesn't exist
     Base.metadata.create_all(engine)
     # Create a session to query the database
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     session = Session()
     # Query all states containing 'a' and order by id
     states_with_a = session.query(State) \
-                                        .filter(State.name.like('%a%')) \
-                                        .order_by(State.id)
+                           .filter(State.name.like('%a%')) \
+                           .order_by(State.id)
 
     # Print out the states that contain 'a'
     for state in states_with_a:
