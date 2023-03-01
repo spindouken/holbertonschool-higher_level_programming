b09f27abe899cd54ff05532a4e517a0cd3cef140
spindouken <mlc9181209@gmail.com>
2023-02-28 19:58:56 -0600

    readability and comments update

--- 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-"""Script that deletes all State objects with a name
-containing letter a from the database hbtn_0e_6_usa"""
+"""
+Script that deletes all State objects with a name
+containing letter 'a' from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    # create an engine for our MySQL database with the given user credentials
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # create all tables in the engine's metadata
+    Base.metadata.create_all(engine)
+
+    # create a session factory bound to the engine
+    Session = sessionmaker(bind=engine)
+
+    # create a session instance
     session = Session()
-    res = session.query(State).filter(State.name.like('%a%')) \
-                 .delete(synchronize_session='fetch')
+
+    # query all State objects with names containing letter 'a', and delete them
+    # (with option to synchronize the session with the database)
+    result = session.query(State).filter(State.name.like('%a%')) \
+                    .delete(synchronize_session='fetch')
+
+    # commit the transaction
     session.commit()
+
+    # close the session
     session.close()
--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,16 +6,22 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3], pool_pre_ping=True))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    # Creates an engine instance with the provided credentials
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Creates all the tables in the database (in this case, State and City)
+    Base.metadata.create_all(engine)
+    # Creates a session to connect to the database
+    Session = sessionmaker(bind=engine)
     session = Session()
+    # Queries all State and City objects that have the same state_id,
+    # and orders by City id
     res = session.query(State, City).filter(State.id == City.state_id) \
-                 .order_by(City.id).all()
-    for states, city in res:
-        print(f"{states.name}: ({city.id}) {city.name}")
+                                    .order_by(City.id).all()
+    # Prints out the name of each state and the corresponding city name and id
+    for state, city in res:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    # Closes the session
     session.close()
