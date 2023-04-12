bad84d4381fca4aa171d90a20747ae7e8b1e6140
spindouken <mlc9181209@gmail.com>
2023-04-11 20:09:59 -0500

    simplify attempt

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+const Sq = require('./5-square.js');
 
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
+class Square extends Sq {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c);
-      }
-      console.log();
+      console.log(c.repeat(this.width));
     }
   }
 }
+
 module.exports = Square;
