f35495c4872fd80413480d44b5a32379ab01a91c
spindouken <mlc9181209@gmail.com>
2023-04-14 11:25:19 -0500

    jquery beginnings

---
+++ 0x03-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+document.querySelector("header").style.color = '#FF0000';
+/*select the first <header> element in the document
+using the document.querySelector() method,
+then set its color property to '#FF0000' (red)
+*/
