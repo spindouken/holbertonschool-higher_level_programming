eb26f86fd461ef706b429a8c834408d60dcbfaab
spindouken <mlc9181209@gmail.com>
2023-04-14 09:56:54 -0500

    working local code push

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const api = process.argv[2];
+const targetID = 18;
+
+request(api, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  let count = 0;
+
+  films.forEach((film) => {
+    if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${targetID}/`)) {
+      count++;
+    }
+  });
+
+  console.log(count);
+});
